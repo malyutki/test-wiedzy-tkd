d70a94c0a4ca5e4f0746267958373317b737c92e
Maurycy Bieganski <maurycy.bieganski@digpro.se>
2021-06-16 14:17:03 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const btn = document.getElementById('random');
+let content = document.getElementById('content');
+const questions = data;
+
+let keys = Object.keys(data[0]);
+
+
+btn.addEventListener('click', () => {
+
+content.innerHTML = '';
+
+for (let step = 0; step < 3; step++) {
+let q1 = getRandomInt(keys.length);
+	let question = keys[q1];
+	let answer = data[0][keys[q1]];
+	
+	if(question === undefined) {content.innerHTML += '<div class="alert alert-danger">koniec pytań</div>';break;}
+	
+	keys.splice(q1, 1);
+	content.innerHTML += `
+	<div class="card mb-2">
+	<div class="card-header">Pytanie nr. ${step+1} </div>
+	<div class="card-body">  
+	<h5 class="card-title">${question}</h5>
+    <p class="card-text d-none" data-index="${step}">${answer.join("<br>")}</p>
+    <button type="button" data-index="${step}" class="btn btn-primary answer">Pokaż/ Ukryj</button>
+  </div>
+</div>`;
+
+let btnAsw = document.querySelectorAll('button.answer');
+
+btnAsw.forEach((el) => {
+	el.addEventListener('click', () => {
+		let ind = el.dataset.index;
+		document.querySelector('p[data-index="' + ind + '"]').classList.toggle('d-none');
+	});
+});
+
+}
+});
+
+
+
+
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
---
+++ js/pytania.js
@@ -0,0 +1,23 @@
+data = [{
+"Wymień zasady Taekwon-Do" : [
+"1. Grzeczność (uprzejmość)",
+"2. Uczciwość (rzetelność)",
+"3. Wytrwałość",
+"4. Samokontrola",
+"5. Niezłomny duch (odwaga)"
+],
+"Jak brzmi przysięga Taekwon-Do" : [
+"1. Będę przestrzegał zasad Taekwon-Do",
+"2. Bedę szanował instruktora i osoby starsze stopniem",
+"3. Nie będę nadużywał technik Taekwon-Do",
+"4. Będę rzecznikiem wolności i sprawiedliwości",
+"5. Będę budował pokój na świecie"
+],
+"Wymień kolory pasów w Taekwon-Do i powiedz co oznacza dany kolor" : [
+"1. Pas biały symbolizuje niewinność, brak wiedzy i doświadczenia w Taekwon-Do",
+"2. Pas żółty jest symbolem ziemi, w której roślina zapuszcza korzenie, jest to początek drogi studenta",
+"3. Pas zielony tak jak roślina zaczyna się rozwijać, tak i umiejętności studenta Taekwon-Do są dużo większe.",
+"4. Pas niebieski jest symbolem nieba, w kierunku którego roślina wzrasta, tak też postępuje trening studenta",
+"5. Pas czerwony oznacza niebezpieczeństwo, umiejętności studenta są na tyle zaawansowane, że stanowią zagrożenie dla ewentualnych przeciwnik"
+]
+}];
